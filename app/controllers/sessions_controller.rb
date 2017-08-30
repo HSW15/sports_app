6251140a9893bc093d0b83b119880167a5c98e6a
JaganNarayanan <jns405@nyu.edu>
2017-08-30 12:48:19 +0800

    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
    def destroy
      session[:user_id] = nil
-     flash[:notice] = "You have successfully logged out, please visit us again!"
+     flash[:success] = "You have successfully logged out, please visit us again!"
      redirect_to root_path
    end
 end
